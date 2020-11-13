382db981e75273f10566da46696a14d3855c13b3
DEV <dev@info.local>
2020-11-13 10:35:08 -0300

    Primeira exercicio da atividade finalizado

---
+++ Atividade 13.11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atividade_13._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+            
+            int Meses = (idade * 12);
+            int dias = (idade * 365);
+            int horas = (dias * 24);
+            int minutos = (horas * 60);
+
+            System.Console.WriteLine("A sua idade em meses é "+Meses);
+            System.Console.WriteLine("A sua idade em dias é "+dias);
+            System.Console.WriteLine("A sua idade em horas é "+horas);
+            System.Console.WriteLine("A sua idade em minutos é "+minutos);
+
+        
+           
+        }
+    }
+}
