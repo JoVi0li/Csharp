4ccff65c30649f2dc966ac0fd117d7cb5d60896c
Your Name <you@example.com>
2020-11-17 13:31:35 -0300

    Atividade finalizada

---
+++ Laços de Repetição/Atividade 17.11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atividade_17._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite um número inteiro");
+            int valor = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ok!");
+            Console.WriteLine("Essa é a tabuada");
+            Console.WriteLine("------------------");
+            int tabuada = 1;
+            int resultado;
+            
+
+            while(tabuada < 11){
+                resultado = valor * tabuada;
+                Console.WriteLine (resultado);  
+                tabuada = tabuada + 1;
+            }
+        }
+    }
+}
