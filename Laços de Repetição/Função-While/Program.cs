556c9dae9c12d526004366fd09ef7c5bd050dbf7
Your Name <you@example.com>
2020-11-17 11:58:47 -0300

    Exemplos da função WHILE

---
+++ Laços de Repetição/Função-While/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Função_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Aprendendo WHILE");
+            // Console.WriteLine("Digite um número");
+            // decimal numero = decimal.Parse (Console.ReadLine());
+            // decimal i = 0;
+            // //WHILE repete enquanto a condição for verdadeira
+            // while(i < numero){
+            //     Console.WriteLine(i);
+            //     i = i + 0.01m; //Incremento
+            // }
+
+            // string resposta = "";
+            // do{
+            //     Console.WriteLine("Digite o seu e-mail");
+            //     resposta = Console.ReadLine();
+            // }while(resposta != "Jovi.oli04@gmail.com");
+            // Console.WriteLine("E-Mail confirmado");
+
+            // Console.WriteLine("Digite a idade do ator");
+            // int idade = int.Parse(Console.ReadLine());
+            // while(idade != 47);{
+            //     Console.WriteLine("Errou!");
+            //     Console.WriteLine("Digite a idade do ator novamente");
+            //     idade = int.Parse(Console.ReadLine());
+            // }
+            // Console.WriteLine("Acertou!");
+
+            // int idade;
+            // do{
+            //     Console.WriteLine("Digite a idade do ator");
+            //     idade = int.Parse(Console.ReadLine());
+            // }while(idade != 47);
+            // Console.WriteLine("Acertou!!!")
+
+            // for (var contador = 0; contador < 10; contador++)
+            // {
+            //     Console.WriteLine("Digite um valor");
+            //     int valor = int.Parse(Console.ReadLine());
+            //     Console.WriteLine($"Você digitou: {valor}");
+            // }
+        }
+    }
+}
