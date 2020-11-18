159ebcf8fd4c3bae44fe1c8c518060e47eaa6ad5
Your Name <you@example.com>
2020-11-18 10:25:34 -0300

    Atividade finalizada

---
+++ Atividades 18.11/Atividade 01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Atividade_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma nota entre 0 e 10:");
+            float nota = float.Parse(Console.ReadLine());
+
+            if (nota >10 || nota <0){
+                Console.WriteLine("Nota Inválida!");
+                Console.WriteLine("Digite a nota novamente:");
+                nota = float.Parse(Console.ReadLine());
+
+            }
+                Console.WriteLine("Nota válida, obrigado!");
+        }
+    }
+}
