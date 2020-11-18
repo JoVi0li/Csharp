61b5e1d647f4e9da650dc36230a07fcde41957a3
Your Name <you@example.com>
2020-11-18 16:52:51 -0300

    Atividade finalizada

---
+++ Atividades 18.11/Desafio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numAnt = 0;
+            int numPos = 1;
+            int sequencia;
+
+            Console.WriteLine("Deseja iniciar a sequência?");
+            string resposta = Console.ReadLine();
+            if( resposta == "sim"){
+                for (var i = 0; i <= 13; i++)
+                {
+                    sequencia = numAnt + numPos; 
+                    Console.WriteLine  (sequencia);
+                    numAnt = numPos; 
+                    numPos = sequencia;   
+                }
+            }else{
+                Console.WriteLine("Ok, o programa será encerrado!");
+            }
+        }
+    }
+}
