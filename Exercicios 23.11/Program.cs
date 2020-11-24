fe02706851500c7fef60d255b30255203b860c3e
Your Name <you@example.com>
2020-11-24 19:01:49 -0300

    Finalizado

---
+++ Exercicios 23.11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicios_23._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de dados
+            Console.WriteLine("Qual o ano do seu nascimento?");
+            int nascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Agora, em que ano nós estamos?");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+            //Processamento
+            int idade = anoAtual - nascimento;
+
+            //Exibição
+            if (idade>=16)
+            {
+                Console.WriteLine("Você está pode votar!");
+            }else{
+                Console.WriteLine("Você não pode votar!");
+            }
+        }
+    }
+}
