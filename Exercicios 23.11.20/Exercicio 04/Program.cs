e5d238e3f846dee032233b753e9e321a3339ad6e
Your Name <you@example.com>
2020-11-25 15:57:50 -0300

    Finalizado

---
+++ Exercicios 23.11.20/Exercicio 04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valor = new int[10];
+            int menor = 2147483647, maior = 0;
+            for (var i = 0; i <10; i++)
+            {
+                Console.WriteLine($"Digite o {i+1}° valor:");
+                valor[i] = int.Parse(Console.ReadLine());
+                if( valor[i] < menor )
+                {
+                    menor = valor[i];
+                }
+                else if( valor[i] > maior )
+                {
+                    maior = valor[i];
+                }
+            }
+                Console.WriteLine($"O maior valor digitado é: {maior}");
+                Console.WriteLine($"O menor valor digitado é: {menor}");
+        }
+    }
+}
---
+++ Exercicios 23.11.20/Exercicio 07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[15];
+
+            Console.WriteLine("Olá");
+            for (var i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"Digite o {i+1}º numero:");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Inverte a sequência dos valores armazenados
+            Array.Reverse(numeros);
+
+            Console.WriteLine("");
+            Console.WriteLine("Esses são os números digitados na ordem inversa:");
+            for (var i = 0; i < 15; i++)
+            {
+                Console.WriteLine("-------------");
+                Console.WriteLine(numeros[i]);
+                Console.WriteLine("-------------");
+
+            }
+        }
+    }
+}
