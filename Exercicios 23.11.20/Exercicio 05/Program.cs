e937f2079137c244ba096a573647cc721723cc49
Your Name <you@example.com>
2020-11-25 11:09:56 -0300

    finalizado

---
+++ Exercicios 23.11.20/Exercicio 05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TABUADA");
+            Console.WriteLine("");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("");
+
+            for (var i = 0; i <= 10; i++){
+                for (var j = 0; j <= 10; j++){
+                    Console.WriteLine($"{i} X {j} = {i*j}");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("");
+            
+            }
+
+            Console.WriteLine("Fim da tabuada");
+            
+        }
+    }
+}
